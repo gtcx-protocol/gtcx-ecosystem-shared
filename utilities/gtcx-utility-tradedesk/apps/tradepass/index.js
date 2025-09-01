cc2eb67e9ce50c6587cd154f9db25cc34e10317c
amani amina anai <iam@amanianai.com>
2025-08-31 20:38:51 -0500

    Massive repository cleanup: remove legacy files, reorganize into focused core/crypto/ui/utilities structure

--- utilities/gtcx-utility-tradedesk/apps/tradepass/index.js
+++
@@ -1,5 +0,0 @@
-// Pin Expo Router to this app's routes directory
-process.env.EXPO_ROUTER_APP_ROOT = './app';
-import 'expo-router/entry';
-
-
--- utilities/gtcx-utility-tradedesk/apps/tradepass/scripts/LoadingViewShim.js
+++
@@ -1,6 +0,0 @@
-// Simple shim to satisfy @expo/metro-runtime LoadingView import on RN 0.76
-export default function LoadingView() {
-  return null;
-}
-
-
